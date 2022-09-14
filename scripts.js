82b0e592ac1611f57b0b533b05bc155405f6952a
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-09-14 21:03:16 +0700

    game logic
    [JS] Wrote out the game logic for winning under checkMatch()

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const debugging = (() => {
     return {print}    
 })();
 
+let gameplaying = true;
 let exPhase = false;
 let ohPhase = true;
 
@@ -46,8 +47,167 @@ const gamestates = (() => {
             }
     }
 
-    return {toggleState}
+    const toggleswitch = document.querySelector('.ticktacky');
+    toggleswitch.addEventListener('click', () => {
+        gamestates.toggleState()
+        switch(ohPhase){
+            case true:
+                toggleswitch.style.backgroundColor="yellow";
+                break;
+            case false:
+                toggleswitch.style.backgroundColor="red";
+                break;
+            default:
+                toggleswitch.style.backgroundColor='white';
+                break;
+        };
+    });
+
+
+    return {toggleState, toggleswitch}
 })();
+
+
+// game logic
+
+const checkMatch = () =>{
+    const square1 = document.querySelector('.square1');
+    const square2 = document.querySelector('.square2');
+    const square3 = document.querySelector('.square3');
+    const square4 = document.querySelector('.square4');
+    const square5 = document.querySelector('.square5');
+    const square6 = document.querySelector('.square6');
+    const square7 = document.querySelector('.square7');
+    const square8 = document.querySelector('.square8');
+    const square9 = document.querySelector('.square9');
+    
+    const row1 = [square1, square2, square3];
+    const row2 = [square4, square5, square6];
+    const row3 = [square7, square8, square9];
+
+    const exmarker = 'exes-game-piece';
+    const exwins = 'Red wins!';
+    const ohmarker = 'ohes-game-piece';
+    const ohwins = 'Yellow wins!';
+    
+    // reds win
+    if(row1[0].classList.contains(exmarker)){
+        if(row1[1].classList.contains(exmarker)){
+            if(row1[2].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    } 
+    if(row2[0].classList.contains(exmarker)){
+        if(row2[1].classList.contains(exmarker)){
+            if(row2[2].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row3[0].classList.contains(exmarker)){
+        if(row3[1].classList.contains(exmarker)){
+            if(row3[2].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row1[0].classList.contains(exmarker)){
+        if(row2[0].classList.contains(exmarker)){
+            if(row3[0].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row1[1].classList.contains(exmarker)){
+        if(row2[1].classList.contains(exmarker)){
+            if(row3[1].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row1[2].classList.contains(exmarker)){
+        if(row2[2].classList.contains(exmarker)){
+            if(row3[2].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row1[0].classList.contains(exmarker)){
+        if(row2[1].classList.contains(exmarker)){
+            if(row3[2].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+    if(row1[2].classList.contains(exmarker)){
+        if(row2[1].classList.contains(exmarker)){
+            if(row3[0].classList.contains(exmarker)){
+                alert(exwins);
+            }
+        }
+    }
+
+
+    // yellows win
+    if(row1[0].classList.contains(ohmarker)){
+        if(row1[1].classList.contains(ohmarker)){
+            if(row1[2].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    } 
+    if(row2[0].classList.contains(ohmarker)){
+        if(row2[1].classList.contains(ohmarker)){
+            if(row2[2].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row3[0].classList.contains(ohmarker)){
+        if(row3[1].classList.contains(ohmarker)){
+            if(row3[2].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row1[0].classList.contains(ohmarker)){
+        if(row2[0].classList.contains(ohmarker)){
+            if(row3[0].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row1[1].classList.contains(ohmarker)){
+        if(row2[1].classList.contains(ohmarker)){
+            if(row3[1].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row1[2].classList.contains(ohmarker)){
+        if(row2[2].classList.contains(ohmarker)){
+            if(row3[2].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row1[0].classList.contains(ohmarker)){
+        if(row2[1].classList.contains(ohmarker)){
+            if(row3[2].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+    if(row1[2].classList.contains(ohmarker)){
+        if(row2[1].classList.contains(ohmarker)){
+            if(row3[0].classList.contains(ohmarker)){
+                alert(ohwins);
+            }
+        }
+    }
+};
+
 
 
 gameboard.populateBoard(gameboard.squareList);
