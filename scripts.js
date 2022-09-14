ec38fd66958cb9995743aa1ea5a7aade56703966
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-09-14 19:52:37 +0700

    Populate gameboard
    [JS] implemented exPhase and ohPhase to switch click effects, organized functions into module

--- scripts.js
+++ scripts.js
@@ -5,11 +5,36 @@ const debugging = (() => {
     return {print}    
 })();
 
+let exPhase = false;
+let ohPhase = true;
 
-const squareList = document.querySelectorAll('.square');
-squareList.forEach(element =>
-    element.addEventListener('click', () => {
-        element.classList.add('selected');
-    }))
+const gameboard = (() => {
+    const squareList = document.querySelectorAll('.square');
+    function populateBoard(nodelist){
+        nodelist.forEach(element =>
+            element.addEventListener('click', () => {
+                if (exPhase){
+                element.classList.add('exes-game-piece')
+            } else if (ohPhase){
+                element.classList.add('ohes-game-piece')
+            };
+            })
+            )
+    }
+    /*
+    squareList.forEach(element =>
+        element.addEventListener('click', () => {
+            if (exPhase){
+            element.classList.add('exes-game-piece')
+        } else if (ohPhase){
+            element.classList.add('ohes-game-piece')
+        };
+        })
+        )*/
+
+    return { populateBoard, squareList }
+})()
 
 
+
+gameboard.populateBoard(gameboard.squareList);
