ca97ffd52f0ff3483b513adceea2247e087984ae
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-09-15 10:54:18 +0700

    Added Xs and Os
    [Asset] Added game pieces
    
    {JS] Added board reset, tried to implement a draw system (not working)
    
    [Issues] Game will periodically (and regularly) add more than one class on click, and that snowballs. Game will also sometimes add a class AFTER the game is over, messing up the next game.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const debugging = (() => {
 
 let exPhase = false;
 let ohPhase = true;
+let drawcount = 0;
 
 const gameboard = (() => {
     const squareList = document.querySelectorAll('.square');
@@ -18,13 +19,29 @@ const gameboard = (() => {
             } else if (ohPhase){
                 element.classList.add('ohes-game-piece')
             };
+            drawcount += 1;
             gamelogic.checkMatch();
             gamestates.toggleState();
-            })
+            console.log(drawcount);
+            console.log(`exPhase: ${exPhase}, ohPhase: ${ohPhase}`)
+            }, { once : true })
             )
-    }
-
-    return { populateBoard, squareList }
+    };
+    
+    // clear the board
+    const resetButton = document.querySelector('#reset-button');
+    const playingfield = document.querySelector('.gameboard');
+    //resetButton.addEventListener('click', (event) => clearBoard(squareList));
+    function clearBoard(nodelist){
+        nodelist.forEach(element => 
+            element.classList.remove('exes-game-piece'));
+        nodelist.forEach(element => 
+            element.classList.remove('ohes-game-piece'));
+        populateBoard(squareList);
+        drawcount = 0;
+    };
+    
+    return { populateBoard, squareList, clearBoard, resetButton }
 })();
 
 const gamestates = (() => {
@@ -36,176 +53,214 @@ const gamestates = (() => {
                 ohPhase = false;
                 exPhase = true;
             }
-    }
-    // this is just not working now
-    const signalState = (phase) => {
-        switch(phase){
-            case true:
-                explayer.style.backgroundColor="yellow";
-                ohplayer.style.backgroundColor="white";
-                break;
-            case false:
-                ohplayer.style.backgroundColor="red";
-                explayer.style.backgroundColor="white";
-                break;
-            default:
-                toggleswitch.style.backgroundColor='white';
-                break;
-        }
-    }
-    const toggleswitch = document.querySelector('.ticktacky');
-    const explayer = document.querySelector('.exes');
-    const ohplayer = document.querySelector('.ohes');
+    }    
 
-    toggleswitch.addEventListener('click', () => {
-        gamestates.toggleState()
-    });
-
-
-    return {toggleState, toggleswitch, signalState}
+    return {toggleState }
 })();
 
 const gamelogic = (() => {
+    
     const checkMatch = () =>{
-    const square1 = document.querySelector('.square1');
-    const square2 = document.querySelector('.square2');
-    const square3 = document.querySelector('.square3');
-    const square4 = document.querySelector('.square4');
-    const square5 = document.querySelector('.square5');
-    const square6 = document.querySelector('.square6');
-    const square7 = document.querySelector('.square7');
-    const square8 = document.querySelector('.square8');
-    const square9 = document.querySelector('.square9');
-    
-    const row1 = [square1, square2, square3];
-    const row2 = [square4, square5, square6];
-    const row3 = [square7, square8, square9];
+        const square1 = document.querySelector('.square1');
+        const square2 = document.querySelector('.square2');
+        const square3 = document.querySelector('.square3');
+        const square4 = document.querySelector('.square4');
+        const square5 = document.querySelector('.square5');
+        const square6 = document.querySelector('.square6');
+        const square7 = document.querySelector('.square7');
+        const square8 = document.querySelector('.square8');
+        const square9 = document.querySelector('.square9');
+        
+        const row1 = [square1, square2, square3];
+        const row2 = [square4, square5, square6];
+        const row3 = [square7, square8, square9];
 
-    const exmarker = 'exes-game-piece';
-    const exwins = 'Red wins!';
-    const ohmarker = 'ohes-game-piece';
-    const ohwins = 'Yellow wins!';
-    
-    // reds win
-    if(row1[0].classList.contains(exmarker)){
-        if(row1[1].classList.contains(exmarker)){
+        const exmarker = 'exes-game-piece';
+        const exwins = "X's win!";
+        const ohmarker = 'ohes-game-piece';
+        const ohwins = "O's win!";
+        
+        
+
+        // ex wins
+        function doesExWin(){
+            if(row1[0].classList.contains(exmarker)){
+                if(row1[1].classList.contains(exmarker)){
+                    if(row1[2].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            } 
+            if(row2[0].classList.contains(exmarker)){
+                if(row2[1].classList.contains(exmarker)){
+                    if(row2[2].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+
+                    }
+                }
+            }
+            if(row3[0].classList.contains(exmarker)){
+                if(row3[1].classList.contains(exmarker)){
+                    if(row3[2].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[0].classList.contains(exmarker)){
+                if(row2[0].classList.contains(exmarker)){
+                    if(row3[0].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[1].classList.contains(exmarker)){
+                if(row2[1].classList.contains(exmarker)){
+                    if(row3[1].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
             if(row1[2].classList.contains(exmarker)){
-                alert(exwins);
+                if(row2[2].classList.contains(exmarker)){
+                    if(row3[2].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[0].classList.contains(exmarker)){
+                if(row2[1].classList.contains(exmarker)){
+                    if(row3[2].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[2].classList.contains(exmarker)){
+                if(row2[1].classList.contains(exmarker)){
+                    if(row3[0].classList.contains(exmarker)){
+                        scoresystem.exscore.innerHTML = Number(scoresystem.exscore.innerHTML) + 1;
+                        alert(exwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
             }
         }
-    } 
-    if(row2[0].classList.contains(exmarker)){
-        if(row2[1].classList.contains(exmarker)){
-            if(row2[2].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row3[0].classList.contains(exmarker)){
-        if(row3[1].classList.contains(exmarker)){
-            if(row3[2].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row1[0].classList.contains(exmarker)){
-        if(row2[0].classList.contains(exmarker)){
-            if(row3[0].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row1[1].classList.contains(exmarker)){
-        if(row2[1].classList.contains(exmarker)){
-            if(row3[1].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row1[2].classList.contains(exmarker)){
-        if(row2[2].classList.contains(exmarker)){
-            if(row3[2].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row1[0].classList.contains(exmarker)){
-        if(row2[1].classList.contains(exmarker)){
-            if(row3[2].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
-    if(row1[2].classList.contains(exmarker)){
-        if(row2[1].classList.contains(exmarker)){
-            if(row3[0].classList.contains(exmarker)){
-                alert(exwins);
-            }
-        }
-    }
 
-
-    // yellows win
-    if(row1[0].classList.contains(ohmarker)){
-        if(row1[1].classList.contains(ohmarker)){
+        // oh wins
+        function doesOhWin(){
+            if(row1[0].classList.contains(ohmarker)){
+                if(row1[1].classList.contains(ohmarker)){
+                    if(row1[2].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row2[0].classList.contains(ohmarker)){
+                if(row2[1].classList.contains(ohmarker)){
+                    if(row2[2].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row3[0].classList.contains(ohmarker)){
+                if(row3[1].classList.contains(ohmarker)){
+                    if(row3[2].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[0].classList.contains(ohmarker)){
+                if(row2[0].classList.contains(ohmarker)){
+                    if(row3[0].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[1].classList.contains(ohmarker)){
+                if(row2[1].classList.contains(ohmarker)){
+                    if(row3[1].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
             if(row1[2].classList.contains(ohmarker)){
-                alert(ohwins);
+                if(row2[2].classList.contains(ohmarker)){
+                    if(row3[2].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[0].classList.contains(ohmarker)){
+                if(row2[1].classList.contains(ohmarker)){
+                    if(row3[2].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            }
+            if(row1[2].classList.contains(ohmarker)){
+                if(row2[1].classList.contains(ohmarker)){
+                    if(row3[0].classList.contains(ohmarker)){
+                        scoresystem.ohscore.innerHTML = Number(scoresystem.ohscore.innerHTML) + 1;
+                        alert(ohwins)
+                        gameboard.clearBoard(gameboard.squareList);
+                    }
+                }
+            } 
+        }
+        
+        // draw
+        function isItADraw(){
+            if(drawcount === 9){
+                alert('Draw');
+                gameboard.clearBoard(gameboard.squareList);
             }
         }
-    } 
-    if(row2[0].classList.contains(ohmarker)){
-        if(row2[1].classList.contains(ohmarker)){
-            if(row2[2].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row3[0].classList.contains(ohmarker)){
-        if(row3[1].classList.contains(ohmarker)){
-            if(row3[2].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row1[0].classList.contains(ohmarker)){
-        if(row2[0].classList.contains(ohmarker)){
-            if(row3[0].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row1[1].classList.contains(ohmarker)){
-        if(row2[1].classList.contains(ohmarker)){
-            if(row3[1].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row1[2].classList.contains(ohmarker)){
-        if(row2[2].classList.contains(ohmarker)){
-            if(row3[2].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row1[0].classList.contains(ohmarker)){
-        if(row2[1].classList.contains(ohmarker)){
-            if(row3[2].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
-    if(row1[2].classList.contains(ohmarker)){
-        if(row2[1].classList.contains(ohmarker)){
-            if(row3[0].classList.contains(ohmarker)){
-                alert(ohwins);
-            }
-        }
-    }
+            
+        
+        doesExWin()
+        doesOhWin()
+        //isItADraw()
+        
     };
+    
+    
+
     return { checkMatch }
 })();
 
+const scoresystem = (() => {
+    const exscore = document.querySelector('.ex-score');
+    const ohscore = document.querySelector('.oh-score');
+
+    return { exscore, ohscore }
+})();
 
 
 gameboard.populateBoard(gameboard.squareList);
